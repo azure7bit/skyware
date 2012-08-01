a7e45a6aa24f3f9d12969c361bce6fd91dfa3e5a
Rahmal Conda <rahmal@gmail.com>
2012-07-31 22:46:28 -0700

    Skywyre: Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skywyre::Application.config.secret_token = 'c98518936d7df7d312058b3c290d472a4c02789691b3e8de1f88206e2394265debb5af9ccc8c226f10589516b2f1559802116e16c79be32c4718398dfa7c1152'
