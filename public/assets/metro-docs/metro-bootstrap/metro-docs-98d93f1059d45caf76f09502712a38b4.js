a413f05e52e83444fbc1e2f7b627f2046eafa94e
Joshua Chestang <joshua.chestang@gmail.com>
2013-11-22 08:50:23 -0600

    removed bootstrap metro

--- public/assets/metro-docs/metro-bootstrap/metro-docs-98d93f1059d45caf76f09502712a38b4.js
+++
@@ -1,5 +0,0 @@
-/* =====================================================
- * NOTE: THIS JAVASCRIPT IS ONLY TO BE USED IN OUR DOCS.
- * DO NOT INCLUDE THIS ON YOUR APP.
- * ===================================================== */
-!function(e){e(function(){e(":submit").click(function(e){e.preventDefault()})})}(window.jQuery);
