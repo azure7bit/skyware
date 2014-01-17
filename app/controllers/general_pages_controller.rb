214654f7ec29eddb1de8e0078b4901f1aa53ada2
Sasi kumar <sasi.madhav2005@gmail.com>
2014-01-17 19:42:41 +0530

    website page refactored

--- app/controllers/general_pages_controller.rb
+++ app/controllers/general_pages_controller.rb
@@ -1,31 +1,31 @@
 class GeneralPagesController < ApplicationController
-  
+  layout 'website'  
   def about
-    render 'general_pages/about', :layout => false
+    render 'general_pages/about'
   end
   
   def contact
-    render 'general_pages/contact', :layout => false
+    render 'general_pages/contact'
   end
   
   def faq
-    render 'general_pages/faq', :layout => false
+    render 'general_pages/faq'
   end
   
   def testimonials
-    render 'general_pages/testimonials', :layout => false
+    render 'general_pages/testimonials'
   end
   
   def pricing
-    render 'general_pages/pricing', :layout => false
+    render 'general_pages/pricing'
   end
   
   def services
-    render 'general_pages/services', :layout => false
+    render 'general_pages/services'
   end
   
   def tos
-    render 'general_pages/tos', :layout => false
+    render 'general_pages/tos'
   end
   
 end
