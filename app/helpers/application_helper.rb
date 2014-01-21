affe95c5126448f385c7d12aaeddfa67b4123c80
Sasi kumar <sasi.madhav2005@gmail.com>
2014-01-21 17:21:42 +0530

    Flash messages in devise pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+	def flash_messages(opts = {})
+		# raise
+		alert_type = {success: :success, error: :danger, notice: :info, alert: :danger}
+		flash.each do |msg_type, text|
+			concat(
+				content_tag(:div, class: "alert alert-#{alert_type[msg_type]} alert-dismissable") do
+					'<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>'.html_safe + text
+				end
+			)
+		end
+		nil
+	end
 end
