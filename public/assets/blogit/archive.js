011d7a75c9ca7234de7b085827e6c43680d7e8c2
HTP <htp@htp-desktop.(none)>
2014-02-06 20:12:29 +0530

    remove the assest folder

--- public/assets/blogit/archive.js
+++
@@ -1,9 +0,0 @@
-// Supporting functions for the display of post archives
-
-jQuery(function(){
-  $("a[data-blogit-click-to-toggle-children]").click(function(event){
-    event.preventDefault();
-    $(this).siblings('ul').toggle();
-  })
-})
-;
