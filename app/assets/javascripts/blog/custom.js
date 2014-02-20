d507883133f7d65da5f9d1c0efd089bf1096b068
Ramli <henryss_10@yahoo.com>
2014-02-20 16:28:41 +0700

    make some improvement for post feature

--- app/assets/javascripts/blog/custom.js
+++ app/assets/javascripts/blog/custom.js
@@ -3,6 +3,7 @@ $(function(){
 	var toggleSidebar = $("#togglesidebar");
 	var primary = $("#primary");
 	var secondary = $("#secondary");
+	var window_height = $(window).height();
 
 	toggleSidebar.on("click", function(){
 
@@ -16,21 +17,33 @@ $(function(){
 			primary.addClass("col-sm-9");
 			secondary.css('display', 'inline-block');
 		}
+
+		if(window_height < secondary.height()){
+			secondary.css("overflow", "auto");
+		}
+		else{
+			secondary.css("overflow", "visible");
+		}
 	});
 
 	var toggleSidebar = $("#togglesidebar-1");
 
 	toggleSidebar.on("click", function(){
 
-		if(primary.hasClass("col-sm-8")){
-			primary.removeClass("col-sm-8");
+		if(primary.hasClass("col-sm-9")){
+			primary.removeClass("col-sm-9");
 			primary.addClass("col-sm-12");
 			secondary.css('display', 'none');
 		}
 		else {
 			primary.removeClass("col-sm-12");
-			primary.addClass("col-sm-8");
+			primary.addClass("col-sm-9");
 			secondary.css('display', 'inline-block');
+		}
+
+		if($(window).width() <= 1366 && primary.hasClass("col-sm-9")){
+			primary.removeClass("col-sm-9");
+			primary.addClass("col-sm-8");
 		}
 	});
 
