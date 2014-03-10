d823f4118d246c49d9c18a5b7126a3344f1727b3
3lviend <henryss_10@yahoo.com>
2014-03-10 17:03:17 +0700

    fix more less content text

--- app/assets/javascripts/blog/custom.js
+++ app/assets/javascripts/blog/custom.js
@@ -59,7 +59,7 @@ $(document).ready(function() {
     $('.more').each(function() {
         var content = $(this).html();if(showChar < content.length) {
             var c = content.substr(0, showChar);
-            var h = content.substr(showChar-1, content.length - showChar);
+            var h = content.substr(showChar, content.length - showChar);
  
             var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
  
@@ -86,7 +86,7 @@ $(document).ready(function() {
             $(this).html(lesstext);
         }
         $(this).parent().prev().toggle();
-        $(this).prev().toggle("slow");
+        $(this).prev().toggle();
         return false;
     });
 });
