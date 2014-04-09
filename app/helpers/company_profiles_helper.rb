28ee7671c8475b6d1f1b90c670ea09544f6da539
Asep Saepudin <asevenfold7bit@gmail.com>
2014-04-09 07:47:15 +0700

    fixing bug

--- app/helpers/company_profiles_helper.rb
+++ app/helpers/company_profiles_helper.rb
@@ -1,9 +1,10 @@
 module CompanyProfilesHelper
   def current_user_picture
+  	@user = @user ? @user : current_user
     if current_business_user
-      url = current_user.avatar.blank? ? "business-avatar.png" : current_user.avatar.url
-    else
-      url = current_user.avatar.blank? ? "img/profile-placeholder.png" : current_user.avatar.url
+      url = @user.avatar.blank? ? "business-avatar.png" : @user.avatar.url
+    elsif current_user
+      url = @user.avatar.blank? ? "img/profile-placeholder.png" : @user.avatar.url
     end
   end
 end
